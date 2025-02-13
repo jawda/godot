734fa9839c01ef5095b923b28a3d1c53a45e6210
jawda <jawda1210@gmail.com>
2025-02-12 23:34:57 -0600

    Sword Bug fix
    tighten up attack hit boxes

--- aarpg/Player/Scripts/state_attack.gd
+++ aarpg/Player/Scripts/state_attack.gd
@@ -11,7 +11,7 @@ var attacking : bool = false
 @onready var walk: State = $"../Walk"
 @onready var attack_anim: AnimationPlayer = $"../../Sprite2D/AttackEffectSprite/AnimationPlayer"
 @onready var audio: AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
-@onready var hurt_box: HurtBox = $"../../Interactions/HurtBox"
+@onready var hurt_box: HurtBox = %AttackHurtBox
 
 
 ## What happens when the player enters this state?
