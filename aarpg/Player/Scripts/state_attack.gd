2883c5056cc66ad018ae2075df00b5ffd8fded9c
jawda <jawda1210@gmail.com>
2025-02-12 22:29:58 -0600

    Player audio and shadow
    added audio and little shadow under character

--- aarpg/Player/Scripts/state_attack.gd
+++ aarpg/Player/Scripts/state_attack.gd
@@ -1,18 +1,29 @@
 class_name State_Attack extends State
 
+## used to keep up with if currently attacking
+var attacking : bool = false
+
+@export var attack_sound: AudioStream
+@export_range(1,20,0.5) var decelerate_speed : float = 5.0
+
 @onready var animation_player: AnimationPlayer = $"../../AnimationPlayer"
 @onready var idle: State = $"../Idle"
 @onready var walk: State = $"../Walk"
 @onready var attack_anim: AnimationPlayer = $"../../Sprite2D/AttackEffectSprite/AnimationPlayer"
+@onready var audio: AudioStreamPlayer2D = $"../../Audio/AudioStreamPlayer2D"
 
-## used to keep up with if currently attacking
-var attacking : bool = false
+
 ## What happens when the player enters this state?
 func Enter() -> void:
 	player.UpdateAnimation("attack")
 	attack_anim.play("attack_" + player.AnimDirection() )
 	## connect to a signal for the animation
 	animation_player.animation_finished.connect( EndAttack )
+	audio.stream = attack_sound
+	## add some audio attack variance
+	audio.pitch_scale = randf_range( 0.9, 1.1 )
+	audio.play()
+	
 	attacking = true
 	pass
 
@@ -26,7 +37,7 @@ func Exit() -> void:
 
 ## What happens during the _process update in this state?
 func Process( _delta: float ) -> State:
-	player.velocity = Vector2.ZERO
+	player.velocity -= player.velocity * decelerate_speed * _delta
 	
 	if attacking == false:
 		if player.direction == Vector2.ZERO:
