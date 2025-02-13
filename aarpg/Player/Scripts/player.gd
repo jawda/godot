269fca40d24eda90684c56261d51aab487d59bb9
jawda <jawda1210@gmail.com>
2025-02-12 23:47:26 -0600

    Update player.gd
    fixing direction bug that was causing moonwalking when pressing multiple directions

--- aarpg/Player/Scripts/player.gd
+++ aarpg/Player/Scripts/player.gd
@@ -1,6 +1,7 @@
 class_name Player extends CharacterBody2D
 
 var cardinal_direction : Vector2 = Vector2.DOWN
+const DIR_4 = [ Vector2.RIGHT, Vector2.DOWN, Vector2.LEFT, Vector2.UP ]
 var direction : Vector2 = Vector2.ZERO
 
 
@@ -32,16 +33,15 @@ func _physics_process( delta ):
 	
 	
 func SetDirection() -> bool:
-	var new_dir : Vector2 = cardinal_direction
+	
 	if direction == Vector2.ZERO:
 		return false
 		
-	#additional checks for controllers
-	if direction.y == 0:
-		new_dir = Vector2.LEFT if direction.x < 0 else Vector2.RIGHT
-	elif direction.x == 0:
-		new_dir = Vector2.UP if direction.y < 0 else Vector2.DOWN
-		
+	## get direction with some fancy math to determine which direction based on the current angle and scew towards cardinal direction
+	## or the direction already facing
+	var direction_id : int = int( round( ( direction + cardinal_direction * 0.1 ).angle() / TAU * DIR_4.size() ) )
+	var new_dir = DIR_4[ direction_id ]	
+	
 	if new_dir == cardinal_direction:
 		return false
 		
