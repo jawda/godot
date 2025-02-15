28639cbeb52e25db8679027b5fa79d8619b9662a
jawda <jawda1210@gmail.com>
2025-02-15 00:08:56 -0600

    Items
    added some item types and handled item effect for healing items (potion and apple)

--- aarpg/GUI/pause_menu/inventory/scripts/inventory_data.gd
+++ aarpg/GUI/pause_menu/inventory/scripts/inventory_data.gd
@@ -3,6 +3,46 @@ class_name InventoryData extends Resource
 ## this will be for the container that holds our inventory items
 @export var slots : Array[ SlotData ]
 
-func add_item() -> void:
+
+func _init() -> void:
+	connect_slots()
+	pass
+
+## needs to check if item in inventory and add it or increase quantity
+func add_item( item : ItemData, count : int = 1) -> bool:
 	
+	## see if item in inventory
+	for s in slots: 
+		if s:
+			if s.item_data == item:
+				s.quantity += count #increase quantity
+				return true
+				
+	## dont have item do have empty slots?
+	for i in slots.size():
+		if slots[ i ] == null:
+			var new = SlotData.new()
+			new.item_data = item
+			new.quantity = count
+			slots[ i ] = new ## add to slot we are at then return
+			new.changed.connect( slot_changed )
+			return true
+			
+	print( "inventory was full!" )
+	return false
+
+func connect_slots() -> void:
+	for s in slots:
+		if s:
+			s.changed.connect( slot_changed )
+			
+func slot_changed() -> void:
+	#everytime a slot changes we check to see if need to remove item because its at zero
+	for s in slots:
+		if s:
+			if s.quantity < 1:
+				s.changed.disconnect( slot_changed )
+				var index = slots.find( s )
+				slots[ index ] = null
+				emit_changed()
 	pass
--- aarpg/GUI/pause_menu/inventory/scripts/inventory_ui.gd
+++ aarpg/GUI/pause_menu/inventory/scripts/inventory_ui.gd
@@ -2,6 +2,8 @@ class_name InventoryUI extends Control
 ## load a ref to the invetory slot
 const INVENTORY_SLOT = preload("res://GUI/pause_menu/inventory/inventory_slot.tscn")
 
+var focus_index : int = 0
+
 @export var data : InventoryData
 
 
@@ -10,6 +12,7 @@ func _ready() -> void:
 	PauseMenu.hidden.connect( clear_inventory )
 	
 	clear_inventory()
+	data.changed.connect( on_inventory_changed )
 	pass
 	
 func clear_inventory() -> void:
@@ -17,10 +20,25 @@ func clear_inventory() -> void:
 		c.queue_free()
 		
 ## create a button and add it to container then assign a slot
-func update_inventory() -> void:
+func update_inventory( i : int = 0) -> void:
 	for s in data.slots:
 		var new_slot = INVENTORY_SLOT.instantiate()
 		add_child( new_slot )
 		new_slot.slot_data = s
+		new_slot.focus_entered.connect( item_focused )
+		
 	
+	await get_tree().process_frame
 	get_child( 0 ).grab_focus()
+
+func item_focused() -> void:
+	for i in get_child_count():
+		if get_child( i ).has_focus():
+			focus_index = i
+			return
+			
+	pass
+func on_inventory_changed() -> void:
+	var i = focus_index
+	clear_inventory()
+	update_inventory()
