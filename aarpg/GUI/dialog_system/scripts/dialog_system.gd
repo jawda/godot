69bd40b8950db51c458e40df259bff9faa7c710c
jawda <jawda1210@gmail.com>
2025-02-27 19:17:12 -0600

    NPC Dialog Choices
    expanding dialog system to have choices

--- aarpg/GUI/dialog_system/scripts/dialog_system.gd
+++ aarpg/GUI/dialog_system/scripts/dialog_system.gd
@@ -7,6 +7,7 @@ signal letter_added( letter : String )
 
 var is_active : bool = false
 var text_in_progress : bool = false
+var waiting_for_choice : bool = false
 
 var text_speed : float = 0.02
 var text_length : int = 0
@@ -23,6 +24,7 @@ var dialog_item_index : int = 0
 @onready var dialog_progress_indicator_label: Label = $DialogUI/DialogProgressIndicator/Label
 @onready var timer: Timer = $DialogUI/Timer
 @onready var audio_stream_player: AudioStreamPlayer = $DialogUI/AudioStreamPlayer
+@onready var choice_options: VBoxContainer = $DialogUI/VBoxContainer
 
 
 
@@ -55,6 +57,8 @@ func _unhandled_input(event: InputEvent) -> void:
 			text_in_progress = false
 			show_dialog_button_indicator( true )
 			return
+		elif waiting_for_choice == true:
+			return
 		dialog_item_index += 1
 		if dialog_item_index < dialog_items.size():
 			start_dialog()
@@ -76,6 +80,7 @@ func show_dialog( _items : Array[ DialogItem ] ) -> void:
 	
 func hide_dialog() -> void:
 	is_active = false
+	choice_options.visible = false
 	dialog_ui.visible = false
 	dialog_ui.process_mode = Node.PROCESS_MODE_DISABLED
 	get_tree().paused = false
@@ -84,9 +89,28 @@ func hide_dialog() -> void:
 	
 # begin the dialog interaction
 func start_dialog() -> void:
+	waiting_for_choice = false
 	show_dialog_button_indicator( false )
 	var _d : DialogItem = dialog_items[ dialog_item_index ]
-	set_dialog_data( _d )
+	
+	if _d is DialogText:
+		set_dialog_text( _d as DialogText )
+	elif _d is DialogChoice:
+		set_dialog_choice( _d as DialogChoice )
+	
+	pass
+	
+# set dialog and NPC variables based on dialog item paramaters.
+# once set start typing timer
+func set_dialog_text( _d : DialogText ) -> void:
+	#content.text = dialog_items[ dialog_item_index ].text
+	
+	content.text = _d.text
+		
+	name_label.text = _d.npc_info.npc_name
+	portrait_sprite.texture = _d.npc_info.portrait
+	portrait_sprite.audio_pitch_base = _d.npc_info.dialog_audio_pitch
+	
 	
 	# hide the text
 	content.visible_characters = 0
@@ -96,6 +120,30 @@ func start_dialog() -> void:
 	start_timer()
 	pass
 	
+#set dialog choice ui based on paramaters
+func set_dialog_choice( _d : DialogChoice ) -> void:
+	choice_options.visible = true
+	waiting_for_choice = true
+	for c in choice_options.get_children():
+		c.queue_free()
+	
+	for i in _d.dialog_branches.size():
+		var _new_choice : Button = Button.new()
+		_new_choice.text = _d.dialog_branches[ i ].text
+		_new_choice.alignment = HORIZONTAL_ALIGNMENT_LEFT
+		_new_choice.pressed.connect( _dialog_choice_selected.bind( _d.dialog_branches[ i ] ) )
+		choice_options.add_child( _new_choice )
+		
+	await get_tree().process_frame	
+	await get_tree().process_frame	
+	choice_options.get_child( 0 ).grab_focus()
+	pass
+
+func _dialog_choice_selected( _d : DialogBranch) -> void:
+	choice_options.visible = false
+	show_dialog( _d.dialog_items )
+	
+	pass
 
 func _on_timer_timeout() -> void:
 	# progress text
@@ -108,17 +156,7 @@ func _on_timer_timeout() -> void:
 		text_in_progress = false
 	pass
 
-func set_dialog_data( _d : DialogItem ) -> void:
-	#content.text = dialog_items[ dialog_item_index ].text
-	if _d is DialogText:
-		content.text = _d.text
-		
-	name_label.text = _d.npc_info.npc_name
-	portrait_sprite.texture = _d.npc_info.portrait
-	portrait_sprite.audio_pitch_base = _d.npc_info.dialog_audio_pitch
-	
-	pass
-	
+
 
 func show_dialog_button_indicator( _is_visible : bool) -> void:
 	dialog_progress_indicator.visible = _is_visible
--- aarpg/Interactables/dialog/scripts/dialog_choice.gd
+++ aarpg/Interactables/dialog/scripts/dialog_choice.gd
@@ -2,4 +2,33 @@
 @icon("res://GUI/dialog_system/icons/question_bubble.svg")
 class_name DialogChoice extends DialogItem
 
-@export var choices : Array[String] = ["Yes" , "No"]
+var dialog_branches : Array[ DialogBranch ]
+
+
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		return
+	
+	for c in get_children():
+		if c is DialogBranch:
+			dialog_branches.append( c )
+	pass
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	# check for dialog
+	if _check_for_dialog_items() == false:
+		return  [ "Requires at least 2 DialogBranch nodes."]
+	else:
+		return []
+	pass
+	
+func _check_for_dialog_items() -> bool:
+	var _count : int = 0
+	for c in get_children():
+		if c is DialogBranch:
+			_count += 1
+			if _count > 1:
+				return true
+	return false
+	
