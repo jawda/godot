5dcea0cdb8445db67ddc6251dab0cba299528035
Joe Williams <joew@quavermusic.com>
2025-04-01 14:31:41 -0500

    feat: drag drop in inventory!

--- aarpg/GUI/pause_menu/inventory/scripts/inventory_slot_ui.gd
+++ aarpg/GUI/pause_menu/inventory/scripts/inventory_slot_ui.gd
@@ -1,6 +1,10 @@
 class_name InventorySlotUI extends Button
 
 var slot_data : SlotData : set = set_slot_data
+var click_pos : Vector2 = Vector2.ZERO
+var dragging : bool = false
+var drag_texture : Control
+var drag_threshold : float = 16.0
 
 @onready var texture_rect: TextureRect = $TextureRect
 @onready var label: Label = $Label
@@ -12,6 +16,18 @@ func _ready() -> void:
 	focus_entered.connect( item_focused )
 	focus_exited.connect( item_unfocused )
 	pressed.connect( item_pressed )
+	button_down.connect( on_button_down )
+	button_up.connect( on_button_up )
+	pass
+	
+func _process(_delta: float) -> void:
+	if dragging:
+		drag_texture.position = get_local_mouse_position() - Vector2( 16, 16)
+		if outside_drag_threshold() == true:
+			drag_texture.modulate.a = 0.5
+		else:
+			drag_texture.modulate.a = 0
+	pass	
 	
 func set_slot_data( value : SlotData ) -> void:
 	slot_data = value
@@ -35,7 +51,7 @@ func item_unfocused() -> void:
 	pass
 
 func item_pressed() -> void:
-	if slot_data:
+	if slot_data and outside_drag_threshold() == false:
 		if slot_data.item_data:
 			var item = slot_data.item_data
 			
@@ -51,3 +67,23 @@ func item_pressed() -> void:
 			if slot_data == null:
 				return # bug fix for null data
 			label.text = str( slot_data.quantity)
+			
+func on_button_down() -> void:
+	click_pos = get_global_mouse_position()
+	dragging = true
+	drag_texture = texture_rect.duplicate()
+	drag_texture.z_index = 10
+	drag_texture.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	add_child( drag_texture )
+	pass
+	
+func on_button_up() -> void:
+	dragging = false
+	if drag_texture:
+		drag_texture.queue_free()
+	pass
+
+func outside_drag_threshold() -> bool:
+	if get_global_mouse_position().distance_to( click_pos ) > drag_threshold:
+		return true
+	return false
