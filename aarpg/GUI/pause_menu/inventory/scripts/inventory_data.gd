251954150aede8f2ae5d5bd6dff1cde714ec6efa
Joe Williams <joew@quavermusic.com>
2025-04-14 12:05:13 -0500

    feat: adding shop purchases

--- aarpg/GUI/pause_menu/inventory/scripts/inventory_data.gd
+++ aarpg/GUI/pause_menu/inventory/scripts/inventory_data.gd
@@ -179,3 +179,11 @@ func get_equipment_bonus( bonus_type : EquipableItemModifier.Type, compare : Equ
 			if m.type == bonus_type:
 				bonus += m.value
 	return bonus
+
+func get_item_held_quantity( _item : ItemData) -> int:
+	for slot in slots:
+		if slot:
+			if slot.item_data:
+				if slot.item_data == _item:
+					return slot.quantity
+	return 0
